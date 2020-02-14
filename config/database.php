1c7cc923f9708e07d0f29ee3ea8c8eebd933e5bf
andriraymond <55683084+andriraymond@users.noreply.github.com>
2020-02-14 21:33:34 +0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+$database_config = [
+    'database_type' => 'pgsql',
+    'server' => 'localhost',
+    'database_name' => 'fruitshop_db',
+    'username' => 'fsuser',
+    'password' => 'fsuser0000'
+];
